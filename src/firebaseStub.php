92db76f5743f41b7fe218915793f4d33caff8bd7
Kyle Paul <kpaul86+github@gmail.com>
2016-09-16 15:13:17 -0500

    Update firebaseStub.php

--- src/firebaseStub.php
+++ src/firebaseStub.php
@@ -1,5 +1,7 @@
 <?php
-namespace DataService;
+namespace Firebase;
+
+require_once __DIR__ . '/firebaseInterface.php';
 
 /**
  * Class FirebaseStub
@@ -75,7 +77,7 @@ class FirebaseStub implements FirebaseInterface
      */
     public function set($path, $data)
     {
-      return $this->_getSetResponse($data);
+        return $this->_getSetResponse($data);
     }
 
     /**
@@ -85,7 +87,7 @@ class FirebaseStub implements FirebaseInterface
      */
     public function push($path, $data)
     {
-      return $this->set($path, $data);
+        return $this->set($path, $data);
     }
 
     /**
@@ -95,7 +97,7 @@ class FirebaseStub implements FirebaseInterface
      */
     public function update($path, $data)
     {
-      return $this->set($path, $data);
+        return $this->set($path, $data);
     }
 
     /**
@@ -104,7 +106,7 @@ class FirebaseStub implements FirebaseInterface
      */
     public function get($path)
     {
-      return $this->_getGetResponse();
+        return $this->_getGetResponse();
     }
 
     /**
@@ -113,7 +115,7 @@ class FirebaseStub implements FirebaseInterface
      */
     public function delete($path)
     {
-      return $this->_getDeleteResponse();
+        return $this->_getDeleteResponse();
     }
 
     /**
@@ -128,23 +130,23 @@ class FirebaseStub implements FirebaseInterface
      * @uses $this->_baseURI
      * @return Error
      */
-    private function _isBaseURIValid() {
-      $error = preg_match('/^https:\/\//', $this->_baseURI);
-      return new Error(($error == 0 ? true : false), 'Firebase does not support non-ssl traffic. Please try your request again over https.');
+    private function _isBaseURIValid()
+    {
+        $error = preg_match('/^https:\/\//', $this->_baseURI);
+        return new Error(($error == 0 ? true : false), 'Firebase does not support non-ssl traffic. Please try your request again over https.');
     }
 
     /**
      * @param $data
      * @return Error
      */
-    private function _isDataValid($data) {
-
-      if ($data == "" || $data == null) {
-        return new Error(true, "Missing data; Perhaps you forgot to send the data.");
-      }
-
-      $error = json_decode($data);
-      return new Error(($error ? false : true), "Invalid data; couldn't parse JSON object, array, or value. Perhaps you're using invalid characters in your key names.");
+    private function _isDataValid($data)
+    {
+        if ($data == "" || $data == null) {
+            return new Error(true, "Missing data; Perhaps you forgot to send the data.");
+        }
+        $error = json_decode($data);
+        return new Error(($error !== null ? false : true), "Invalid data; couldn't parse JSON object, array, or value. Perhaps you're using invalid characters in your key names.");
     }
 
     /**
@@ -153,17 +155,17 @@ class FirebaseStub implements FirebaseInterface
      */
     private function _getSetResponse($data)
     {
-      $validBaseUriObject = $this->_isBaseURIValid();
-      if ($validBaseUriObject->error) {
-        return $validBaseUriObject->message;
-      }
+        $validBaseUriObject = $this->_isBaseURIValid();
+        if ($validBaseUriObject->error) {
+            return $validBaseUriObject->message;
+        }
 
-      $validDataObject = $this->_isDataValid($data);
-      if ($validDataObject->error) {
-        return $validDataObject->message;
-      }
+        $validDataObject = $this->_isDataValid($data);
+        if ($validDataObject->error) {
+            return $validDataObject->message;
+        }
 
-      return $this->_response;
+        return $this->_response;
     }
 
     /**
@@ -171,31 +173,36 @@ class FirebaseStub implements FirebaseInterface
      */
     private function _getGetResponse()
     {
-      $validBaseUriObject = $this->_isBaseURIValid();
-      if ($validBaseUriObject->error) {
-        return $validBaseUriObject->message;
-      }
-      return $this->_response;
+        $validBaseUriObject = $this->_isBaseURIValid();
+        if ($validBaseUriObject->error) {
+            return $validBaseUriObject->message;
+        }
+        return $this->_response;
     }
 
     /**
      * @return null
      */
-    private function _getDeleteResponse() { return $this->_getGetResponse(); }
+    private function _getDeleteResponse()
+    {
+        return $this->_getGetResponse();
+    }
 }
 
 /**
  * Class Error
+ *
  * @package Firebase
  */
-class Error {
+class Error
+{
     /**
      * @param $error
      * @param $message
      */
     function __construct($error, $message)
-  {
-    $this->error = $error;
-    $this->message = $message;
-  }
+    {
+        $this->error = $error;
+        $this->message = $message;
+    }
 }
